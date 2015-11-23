07aca5bfa819b48c61b8ebd6c827627cfc38ff89
Taity-mini <taitymini@hotmail.co.uk>
2015-11-23 14:34:59 +0000

    Script #2 with added HTML

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var port = process.env.PORT || 1337;
 
 var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.write('<!DOCTYPE html>');
+    response.write('<!DOCTYPE  html>');
     response.write("<html>");
     response.write("<head>");
     response.write("<title>Hello World Page</title>");
