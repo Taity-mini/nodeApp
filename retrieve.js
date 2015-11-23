e8b54ec659c4cd6de0de3d89ba6c4fb5f5369344
Taity-mini <taitymini@hotmail.co.uk>
2015-11-23 15:29:18 +0000

    Display records from MongoDB

--- retrieve.js
+++ retrieve.js
@@ -47,6 +47,10 @@ http.createServer(function(request, response) {
                     db.close();
                 }
             });
+            //Done Close connection
+            db.close();
+        }
+        response.end('Finished, Connection closed \n');
+    });
 
-
-        }).listen(port);
+}).listen(port);
